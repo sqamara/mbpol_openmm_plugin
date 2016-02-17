98e8f457cd2e26506817486e3284f131ec3dbe01
Sebastian <sqamara@ucsd.edu>
2016-02-17 15:53:57 -0800

    electrostatics passes tests

--- python/tests/Test_i-TTM_elec.py
+++ python/tests/Test_i-TTM_elec.py
@@ -33,10 +33,10 @@ class TestCustomForce(unittest.TestCase):
         state = simulation.context.getState(getForces=True, getEnergy=True, getPositions=True)
         potential_energy = state.getPotentialEnergy()
         potential_energy.in_units_of(unit.kilocalorie_per_mole)
-        print("calculated energy = {}  expected energy = {}".format(potential_energy.in_units_of(unit.kilojoule_per_mole)._value, expected_energy))
+        print("calculated energy = {}  expected energy = {}".format(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value, expected_energy))
         
         
-        self.assertTrue(abs(potential_energy.in_units_of(unit.kilojoule_per_mole)._value - expected_energy) < .01)
+        self.assertTrue(abs(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value - expected_energy) < .1)
     def test_water_br1(self):
         self.test_water_br(pdb_file="./pdb_files/water_br21.pdb", expected_energy=2.75913036)
     def test_Cl_Na(self):
